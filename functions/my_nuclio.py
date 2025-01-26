507511aebd14eee6f20c4e49d6fdd1d7506573cc
amit-elbaz <125362803+amit-elbaz@users.noreply.github.com>
2025-01-26 13:02:43 +0200

    Update my_nuclio.py

--- functions/my_nuclio.py
+++ functions/my_nuclio.py
@@ -2,8 +2,8 @@ import mlrun
 
 
 def handler(context, event):
-    #project = mlrun.get_or_create_project("nuclio-trigger-workflow-amite", "../")
-    project = mlrun.get_current_project()
+    project = mlrun.get_or_create_project("nuclio-trigger-workflow-amite", "../")
+    # project = mlrun.get_current_project()
     workflow_instance = project.run(
                             name="my-workflow",
                             watch=False,
