f2de31a1ad1c88649f6b0d69e38ddf9b2a00a73b
amit-elbaz <amite@iguazio.com>
2025-01-26 08:11:05 +0000

    add files

---
+++ functions/my_nuclio.py
@@ -0,0 +1,12 @@
+import mlrun
+
+
+def handler(context, event):
+    project = mlrun.get_or_create_project(name="nuclio-trigger-workflow-amite")
+    workflow_instance = project.run(
+                            name="my-workflow",
+                            watch=False,
+                            # local=False,
+                            engine="kfp",
+                    )
+    
--- workflows/workflow.py
+++ workflows/workflow.py
@@ -1,2 +1,15 @@
-def my_workflow():
-    return 1
+
+from kfp import dsl
+from mlrun.platforms import auto_mount
+import os
+import sys
+import mlrun
+
+@dsl.pipeline(name="Decision pipeline", description="Pipeline that runs a function based on ")
+def kfpipeline(input_val):
+
+    
+    step_1 = mlrun.run_function('func_A')    
+
+    step_2 = mlrun.run_function('func_A').after(step_1)
+    
