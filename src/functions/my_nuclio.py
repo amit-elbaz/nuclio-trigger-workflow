1f5f8fdc331d98b07898097e6e1e11000dd84f6f
amit-elbaz <125362803+amit-elbaz@users.noreply.github.com>
2025-01-29 11:24:44 +0200

    Update my_nuclio.py

--- src/functions/my_nuclio.py
+++ src/functions/my_nuclio.py
@@ -1,14 +1,17 @@
 import mlrun
-
-def init_context(context):
-    context.logger.info("start init context")
-    project = mlrun.load_project(url="git://github.com/amit-elbaz/nuclio-trigger-workflow.git#master", context="./my-loaded-project")
-    setattr(context,"project",project)
+# import sys
+# sys.path.append("/opt/nuclio")
+from util.utility import utility_func
+# def init_context(context):
+#     context.logger.info("start init context")
+#     project = mlrun.load_project(url="git://github.com/amit-elbaz/nuclio-trigger-workflow.git#master", context="./my-loaded-project")
+#     setattr(context,"project",project)
     
 def handler(context, event):
     context.logger.info("start handler")
-    # project = mlrun.get_or_create_project("nuclio-trigger-workflow-amite", "/opt/nuclio")
-    # project = mlrun.get_current_project()
-    workflow_instance = context.project.run(name="my-workflow", watch=False, engine="kfp")
+    
+    project = mlrun.get_or_create_project("nuclio-trigger-workflow-amite", "./")
+    workflow_instance = project.run(name="my-workflow", watch=False, engine="kfp", dirty=True)
+    return utility_func()
     # workflow_instance = project.run(workflow_path="/opt/nuclio/workflows/workflow.py")
     
