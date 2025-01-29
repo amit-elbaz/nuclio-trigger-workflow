8df611a3b3a6db169849760e832c82dc50d4a314
amit-elbaz <125362803+amit-elbaz@users.noreply.github.com>
2025-01-29 09:59:35 +0200

    Update my_nuclio.py

--- functions/my_nuclio.py
+++ functions/my_nuclio.py
@@ -1,5 +1,5 @@
 # import mlrun
-from util import utility_func
+# from util import utility_func
 # def init_context(context):
 #     context.logger.info("start init context")
 #     project = mlrun.load_project(url="git://github.com/amit-elbaz/nuclio-trigger-workflow.git#master", context="./my-loaded-project")
@@ -7,7 +7,7 @@ from util import utility_func
     
 def handler(context, event):
     context.logger.info("start handler")
-    return utility_func
+    # return utility_func
     # project = mlrun.get_or_create_project("nuclio-trigger-workflow-amite", "/opt/nuclio")
     # project = mlrun.get_current_project()
     # workflow_instance = context.project.run(name="my-workflow", watch=False, engine="kfp")
