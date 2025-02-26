9e67046bc1648b8e8c8574f99694e823b7a72574
amit-elbaz <125362803+amit-elbaz@users.noreply.github.com>
2025-02-26 13:44:11 +0200

    Update my_nuclio.py

--- functions/my_nuclio.py
+++ functions/my_nuclio.py
@@ -3,18 +3,18 @@ import mlrun
 # sys.path.append("/opt/nuclio")
 from util.utility import utility_func
 
-def init_context(context):
-    context.logger.info("start init context")
-    # project = mlrun.load_project(url="git://github.com/amit-elbaz/nuclio-trigger-workflow.git#master", context="./my-loaded-project")
-    project = mlrun.get_or_create_project("nuclio-trigger-workflow-amite", "./")
+# def init_context(context):
+#     context.logger.info("start init context")
+#     # project = mlrun.load_project(url="git://github.com/amit-elbaz/nuclio-trigger-workflow.git#master", context="./my-loaded-project")
+#     project = mlrun.get_or_create_project("nuclio-trigger-workflow-amite", "./")
 
-    setattr(context,"project",project)
+#     setattr(context,"project",project)
     
 def handler(context, event):
     context.logger.info("start handler")
     
     # project = mlrun.get_or_create_project("nuclio-trigger-workflow-amite", "./")
-    workflow_instance = context.project.run(name="my-workflow", engine="remote")
+    # workflow_instance = context.project.run(name="my-workflow", engine="remote")
     # workflow_instance = context.project.run(name="my-workflow", watch=False, engine="kfp", dirty=True)
     # job = context.project.run_function("func-a")
     return utility_func()
